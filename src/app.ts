2bab71137ed2c5e20593cf9864a1c0ab2bab8a7b
Jhollyfer Rodrigues <jhollyfer.fr@gmail.com>
2023-02-20 04:11:58 -0300

    :ambulance: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import cookie from '@fastify/cookie';
+import cors from '@fastify/cors';
 import fastify from 'fastify';
 
 import { filesRoutes } from './routes/files';
 
 export const app = fastify();
+
+app.register(cors, {});
 
 // app.addHook('preHandler', async (request, reply) => {
 // 	console.log(`[${request.method}] ${request.url}`);
